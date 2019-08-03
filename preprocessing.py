853a3978fe61724ba6dfae1aaeb27cfab21b0844
Dyotty <utk.ysd@gmail.com>
2019-08-03 15:20:53 +0900

    前処理部分をクラス化

--- preprocessing.py
+++ preprocessing.py
@@ -4,115 +4,195 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 
-def preprocessing(data):
-    return data
+class Preprocessing:
 
+    def __init__(self, data, mode, on_off_list):
+        self.data = data
+        self.mode = mode
+        self.on_off_flg = on_off_list
 
-def preprocessing_with_key(key, data):
-    func_dct =\
-        {
-            "Pclass": prepro_Pclass,
-            "Name": prepro_Name,
-            "Sex": prepro_Sex,
-            "Age": prepro_Age,
-            "SibSp": prepro_SibSp,
-            "Parch": prepro_Parch,
-            "Ticket": prepro_Ticket,
-            "Cabin": prepro_Ticket,
-            "Embarked": prepro_Embarked
-        }
-    return func_dct[key](data)
+        # trainとtestでデータ形式が変わる（ラベルがない分）ので調整
+        if mode == "train":
+            self.idx_feature_start = 2
+        elif mode == "test":
+            self.idx_feature_start = 1
 
+        # 特徴名のリスト（後で使う。ここで宣言しないと面倒）
+        self.feature_name_lst = data[0, self.idx_feature_start:]
 
-def prepro_Pclass(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "":
-            dst[i] = -1
-        else:
-            dst[i] = val
-    return dst
+        # 特徴名ごとの列インデックスdictionary
+        self.feature_name_dct = {}
+        for i, name in enumerate(self.feature_name_lst):
+            self.feature_name_dct[name] = i
 
+        # 特徴データが書かれている列以降のみにする
+        self.feature_data = data[1:, self.idx_feature_start:]
 
-def prepro_Name(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "":
-            dst[i] = -1
-        else:
-            dst[i] = val
-    return dst
+    # 新規特徴量
+    # 1人あたりの料金
+    def fare_for_one_person(self):
+        # 必要なデータを前処理
+        sibsp_data = self.preprocessing_with_key("SibSp")
+        parch_data = self.preprocessing_with_key("Parch")
+        fare_data = self.preprocessing_with_key("Fare")
+        # 一人あたりの料金を計算
+        fare_for_one_person = fare_data / (sibsp_data + parch_data + 1)
+        # hstack結合の為に二次元にしておく
+        fare_for_one_person = np.reshape(fare_for_one_person, (-1, 1))
 
+        return fare_for_one_person
 
-def prepro_Sex(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "male":
-            dst[i] = 0
-        elif val == "female":
-            dst[i] = 1
-        else:
-            dst[i] = -1
-    return dst
+    def get_label(self):
+        if self.mode == "train":
+            return np.array(self.data[1:, 1], np.float64)
 
+    def preprocessing(self):
+        # 使用する特徴を切り替える為に特徴名のdictionaryを作成
+        feature_name_dct = {}
+        for i, name in enumerate(self.feature_name_lst):
+            feature_name_dct[name] = i
 
-def prepro_Age(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "":
-            dst[i] = -1
-        else:
-            dst[i] = val
-    return dst
+        # 使用する特徴を切り替える為に、残った特徴名listを作成
+        remain_name_lst = []
+        for i, name in enumerate(self.feature_name_lst):
+            if self.on_off_flg[name]:
+                remain_name_lst.append(name)
 
+        # 使用する特徴のみ残して前処理を行う
+        # 使用する特徴名でループ、前処理を行い結果をappendしていく
+        selected_feature_data_lst = []
+        for name in remain_name_lst:
+            one_feature_data = self.preprocessing_with_key(name)
+            selected_feature_data_lst.append(one_feature_data)
+        selected_feature_data = np.array(selected_feature_data_lst)     # ndarrayに変換
+        selected_feature_data = selected_feature_data.T                 # 転置して(891,*)のshapeにする
 
-def prepro_SibSp(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "":
-            dst[i] = -1
-        else:
-            dst[i] = val
-    return dst
+        # np.float64に変換
+        dst = np.asarray(selected_feature_data, np.float64)
 
+        # 新規特徴量を追加
+        fare_for_one_person = self.fare_for_one_person()
+        dst = np.hstack((dst, fare_for_one_person))
 
-def prepro_Parch(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "":
-            dst[i] = -1
-        else:
-            dst[i] = val
-    return dst
+        # 標準化（スケール統一）
+        stdsc = StandardScaler()
+        dst = stdsc.fit_transform(dst)
 
+        return dst
 
-def prepro_Ticket(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "":
-            dst[i] = -1
-        else:
-            dst[i] = val
-    return dst
+    def preprocessing_with_key(self, key):
+        func_dct =\
+            {
+                "Pclass": self.prepro_Pclass,
+                "Name": self.prepro_Name,
+                "Sex": self.prepro_Sex,
+                "Age": self.prepro_Age,
+                "SibSp": self.prepro_SibSp,
+                "Parch": self.prepro_Parch,
+                "Ticket": self.prepro_Ticket,
+                "Fare": self.prepro_Fare,
+                "Cabin": self.prepro_Ticket,
+                "Embarked": self.prepro_Embarked
+            }
+        return func_dct[key](self.feature_data[:, self.feature_name_dct[key]])
 
+    def prepro_Pclass(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "":
+                dst[i] = -1
+            else:
+                dst[i] = val
+        dst = np.asarray(dst, np.float64)
+        return dst
 
-def prepro_Cabin(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "":
-            dst[i] = -1
-        else:
-            dst[i] = val
-    return dst
+    def prepro_Name(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "":
+                dst[i] = -1
+            else:
+                dst[i] = val
+        return dst
 
+    def prepro_Sex(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "male":
+                dst[i] = 0
+            elif val == "female":
+                dst[i] = 1
+            else:
+                dst[i] = -1
+        dst = np.asarray(dst, np.float64)
+        return dst
 
-def prepro_Embarked(data):
-    dst = np.zeros(data.shapes)
-    for i, val in enumerate(data):
-        if val == "Q":
-            dst[i] = 0
-        if val == "C":
-            dst[i] = 0.5
-        if val == "S":
-            dst[i] = 1
-    return dst
+    def prepro_Age(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "":
+                dst[i] = 24
+            else:
+                dst[i] = val
+        dst = np.asarray(dst, np.float64)
+        return dst
+
+    def prepro_SibSp(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "":
+                dst[i] = 0
+            else:
+                dst[i] = val
+        dst = np.asarray(dst, np.float64)
+        return dst
+
+    def prepro_Parch(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "":
+                dst[i] = 0
+            else:
+                dst[i] = val
+        dst = np.asarray(dst, np.float64)
+        return dst
+
+    def prepro_Fare(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "":
+                dst[i] = 0
+            else:
+                dst[i] = val
+        dst = np.asarray(dst, np.float64)
+        return dst
+
+    def prepro_Ticket(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "":
+                dst[i] = -1
+            else:
+                dst[i] = val
+        return dst
+
+    def prepro_Cabin(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "":
+                dst[i] = -1
+            else:
+                dst[i] = val
+        return dst
+
+    def prepro_Embarked(self, data):
+        dst = np.zeros(data.shape)
+        for i, val in enumerate(data):
+            if val == "Q":
+                dst[i] = 0
+            if val == "C":
+                dst[i] = 0.5
+            if val == "S":
+                dst[i] = 1
+        dst = np.asarray(dst, np.float64)
+        return dst
--- train.py
+++ train.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import os
 import csv
 import numpy as np
-import preprocessing as pp
+from preprocessing import Preprocessing
+import models as mdl
 from sklearn.preprocessing import StandardScaler
 
 from keras.models import Model
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input, Conv1D, MaxPool1D, Flatten, BatchNormalization
+from keras import regularizers
 from keras.utils import np_utils
 from keras.utils import plot_model
+from keras.models import load_model
+from keras.callbacks import TensorBoard
 
 import datetime as dt
+
+# Try Name
+try_name = "MLP_Deep"
+
 
 def read_csv(file_path):
     dst = []
@@ -21,39 +30,106 @@ def read_csv(file_path):
     return np.array(dst)
 
 
-def train_mlp(train_data, label):
-    inputs = Input(shape=train_data.shape[1:])
-    data = Dense(20, activation="relu", kernel_initializer="random_uniform", bias_initializer="constant")(inputs)
-    data = Dense(20, activation="relu", kernel_initializer="random_uniform", bias_initializer="constant")(data)
-    data = Dense(20, activation="relu", kernel_initializer="random_uniform", bias_initializer="constant")(data)
-    data = Dense(2, activation="softmax", kernel_initializer="random_uniform", bias_initializer="constant")(data)
-
-    mlp = Model(inputs, data, name="MLP")
-    mlp.summary()
-    plot_model(mlp, to_file='mlp.png', show_shapes=True)
+def train_mlp(train_data, label, weights="", additional_epoch=100):
+    mlp = mdl.model_construct_mlp_deep(train_data)
 
     mlp.compile(metrics=["accuracy"],
                 optimizer="adam",
                 loss="categorical_crossentropy")
 
-    mlp.fit(train_data, label,
+    logdir = "log/" + try_name + dt.datetime.now().strftime("%Y%m%d-%H:%M:%S")
+    os.mkdir(logdir)
+    callbacks = TensorBoard(log_dir=logdir,
+                            histogram_freq=1,
+                            write_graph=True,
+                            write_grads=True,
+                            write_images=True)
+    cbks = [callbacks]
+
+    if weights != "":
+        mlp = load_model(weights)
+        mlp.fit(train_data, label,
+                batch_size=50,
+                epochs=additional_epoch,
+                shuffle="True",
+                callbacks=cbks,
+                validation_data=(train_data, label)
+                )
+    else:
+        mlp.fit(train_data, label,
+                batch_size=50,
+                epochs=300,
+                shuffle="True",
+                validation_data=(train_data, label),
+                callbacks=cbks
+                # validation_split=0.25
+                )
+
+    mlp.save("MLP.h5")
+
+    return mlp
+
+
+def train_cnn1d(train_data, label):
+    cnn1d = mdl.model_construct_cnn1d(train_data)
+
+    cnn1d.compile(metrics=["accuracy"],
+                optimizer="adam",
+                loss="categorical_crossentropy")
+
+    train_data = np.reshape(train_data, (-1, train_data.shape[1], 1))
+    cnn1d.fit(train_data, label,
             batch_size=50,
-            epochs=300,
+            epochs=00,
             shuffle="True",
             validation_data=(train_data, label)
             )
 
-    now_time = dt.datetime.now()
-    mlp.save("MLP.h5")
+    cnn1d.save("CNN1D.h5")
+
+    return cnn1d
+
+
+def predict_mlp(test_data, model_path):
+    model = load_model(model_path)
+    result = model.predict(test_data)
+
+    return result
+
+
+def predict_cnn1d(test_data, model_path):
+    test_data = np.reshape(test_data, (-1, test_data.shape[1], 1))
+    model = load_model(model_path)
+    result = model.predict(test_data)
+
+    return result
+
+# 学習設定
+on_off_flg =\
+        {
+            "Pclass": True,
+            "Name": False,
+            "Sex": True,
+            "Age": True,
+            "SibSp": True,
+            "Parch": True,
+            "Ticket": False,
+            "Fare": True,
+            "Cabin": False,
+            "Embarked": True
+        }
+model_type = "MLP"
 
 
 # データ読み込み
 train_data_path = "dataset/train.csv"
 train_data = read_csv(train_data_path)
+# データ前処理クラス初期化
+pp = Preprocessing(train_data, mode="train", on_off_list=on_off_flg)
 
 # 特徴量選定
-train_data_selected = pp.preprocessing(train_data)
-label = pp.get_label(train_data)
+train_data_selected = pp.preprocessing()
+label = pp.get_label()
 label = np_utils.to_categorical(label)
 
 # 標準化
@@ -61,4 +137,30 @@ stdsc = StandardScaler()
 train_data_selected = stdsc.fit_transform(train_data_selected)
 
 #
-train_mlp(train_data_selected, label)
+if model_type == "MLP":
+    model = train_mlp(train_data_selected, label)
+elif model_type == "CNN1D":
+    model = train_cnn1d(train_data_selected, label)
+
+#
+test_data_path = "dataset/test.csv"
+test_data = read_csv(test_data_path)
+pp = Preprocessing(test_data, mode="test", on_off_list=on_off_flg)
+
+test_data_selected = pp.preprocessing()
+stdsc = StandardScaler()
+test_data_selected_std = stdsc.fit_transform(test_data_selected)
+
+if model_type == "MLP":
+    result = model.predict(test_data_selected_std)
+elif model_type == "CNN1D":
+    result = predict_cnn1d(test_data_selected_std, "CNN1D.h5")
+result = np.reshape(np.argmax(result, axis=-1), (-1, 1))
+PassengerId = np.reshape(np.array(test_data[1:, 0]), (-1, 1))
+write_data = np.hstack((PassengerId, result))
+
+with open('result.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["PassengerId", "Survived"])
+    for i, _row in enumerate(write_data):
+        writer.writerow(_row)
